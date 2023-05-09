b5e1385c160f5d0c568a574284e39cbb547749f7
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-09 10:24:27 -0500

    Agregando mas dependencias: Jpa, flyway, mysql. Añadiendo la configuracion para la conexion a application.properties. Y creando la Entidad Medico

---
+++ src/main/java/med/meditec/api/medico/Medico.java
@@ -0,0 +1,30 @@
+package med.meditec.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.meditec.api.direccion.Direccion;
+
+//JPA
+@Table(name = "medicos")
+@Entity(name = "Medico")
+//Lombok
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+
+}
