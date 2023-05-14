ad573e597b47c7a3444376feb48085216f414a6b
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-13 22:25:49 -0500

    Agregando un controlador de errores para la excepción EntityNotFoundExecption

--- src/main/java/med/meditec/api/infra/TradadorDeErrores.java
+++ src/main/java/med/meditec/api/infra/TradadorDeErrores.java
@@ -1,2 +1,14 @@
-package med.meditec.api.infra;public class TradadorDeErrores {
+package med.meditec.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TradadorDeErrores {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404() {
+        return ResponseEntity.notFound().build();
+    }
 }
