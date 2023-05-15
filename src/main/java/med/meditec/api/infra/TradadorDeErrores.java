6e4a1f758d7d94e07d99633a71e650875dfb8525
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-15 10:14:19 -0500

    Creando un tratador de errores para el manejor del error 404 y 400

--- src/main/java/med/meditec/api/infra/TradadorDeErrores.java
+++ src/main/java/med/meditec/api/infra/TradadorDeErrores.java
@@ -2,6 +2,8 @@ package med.meditec.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +12,17 @@ public class TradadorDeErrores {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores=e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String field, String defaultMessage) {
+        public  DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
