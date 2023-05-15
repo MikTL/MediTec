a9b3a594265e2cce4e07a2934f2949b24750647e
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-15 11:45:32 -0500

    Creando la entidad Usuario para el acceso y su migracion en flyway

--- src/main/java/med/meditec/api/domain/usuario/Usuario.java
+++ src/main/java/med/meditec/api/domain/usuario/Usuario.java
@@ -1,2 +1,21 @@
-package med.meditec.api.domain.usuario;public class Usuario {
+package med.meditec.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nombre;
+    String clave;
 }
--- src/main/resources/db/migration/V6__create-table-usuarios.sql
+++ src/main/resources/db/migration/V6__create-table-usuarios.sql
@@ -1,14 +1,7 @@
-create table medicos(
+create table usuarios(
     id bigint not null auto_increment,
     nombre varchar(100) not null,
-    email varchar(100) not null unique,
-    documento varchar(8) not null unique,
-    especialidad varchar(100) not null,
-    calle varchar(100) not null,
-    distrito varchar(100) not null,
-    complemento varchar(100),
-    numero varchar(20),
-    ciudad varchar(100) not null,
+    clave varchar(300) not null,
 
     primary key(id)
 );
